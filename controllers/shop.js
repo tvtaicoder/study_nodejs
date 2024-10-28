6b5103f25be38a6c774a11d2b75ac7da3e420037
tvtaicoder <taikaka2015@gmail.com>
2024-10-28 11:57:03 +0700

    complete some code

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,11 +18,6 @@ exports.getProduct = (req, res, next) => {
 
     Product.findById(prodId) // Sử dụng findByPk để tìm sản phẩm theo ID
         .then(product => {
-            // if (!product) {
-            //     // Nếu không tìm thấy sản phẩm, trả về lỗi 404
-            //     return res.status(404).render('404', { pageTitle: 'Product Not Found', path: '/404' });
-            // }
-            // Nếu tìm thấy sản phẩm, render trang chi tiết sản phẩm
             res.render('shop/product-detail', {
                 product: product,
                 pageTitle: product.title,
@@ -47,17 +42,16 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
     req.user.getCart()
-        .then(cart => {
-            return cart.getProducts()
-                .then(
-                    products => {res.render('shop/cart', {
-                        pageTitle: 'Your Cart',
-                        path: '/cart',
-                        products: products
-                    })}).catch(err => console.log(err));
+        .then(products => {
+            res.render('shop/cart', {
+                pageTitle: 'Your Cart',
+                path: '/cart',
+                products: products
+            });
         })
         .catch(err => console.log(err));
-}
+};
+
 
 exports.postCart = (req, res, next) => {
     // Lấy `productId` từ dữ liệu gửi lên thông qua biểu mẫu (POST request)
@@ -66,83 +60,21 @@ exports.postCart = (req, res, next) => {
         .then(product => {
             return req.user.addToCart(product);
         })
+        .then(
+            res.redirect('./cart')
+        )
         .then(result => {
             console.log(result);
         })
         .catch(err =>{
             console.log(err);
         })
-    //
-    // // Biến để lưu trữ giỏ hàng lấy được và số lượng sản phẩm mới
-    // let fetchedCart;
-    // let newQuantity = 1;
-    //
-    // // Lấy giỏ hàng của người dùng hiện tại
-    // req.user.getCart()
-    //     .then(cart => {
-    //         // Gán giỏ hàng vừa lấy vào biến `fetchedCart`
-    //         fetchedCart = cart;
-    //
-    //         // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa bằng cách tìm theo `prodId`
-    //         return cart.getProducts({ where: { id: prodId } });
-    //     })
-    //     .then(products => {
-    //         // Biến để lưu trữ sản phẩm (nếu tìm thấy trong giỏ hàng)
-    //         let product;
-    //
-    //         // Nếu sản phẩm đã tồn tại trong giỏ hàng (mảng `products` có phần tử), gán sản phẩm đó vào biến `product`
-    //         if (products.length > 0) {
-    //             product = products[0]; // Lấy sản phẩm đầu tiên trong mảng (do tìm theo id nên chỉ có 1 sản phẩm)
-    //         }
-    //
-    //         // Nếu sản phẩm đã có trong giỏ hàng
-    //         if (product) {
-    //             // Lấy số lượng cũ của sản phẩm trong giỏ hàng
-    //             const oldQuantity = product.cartItem.quantity;
-    //
-    //             // Tăng số lượng sản phẩm trong giỏ hàng lên 1
-    //             newQuantity = oldQuantity + 1;
-    //
-    //             // Trả về sản phẩm để tiếp tục xử lý
-    //             return product;
-    //         }
-    //
-    //         // Nếu sản phẩm chưa có trong giỏ hàng, tìm sản phẩm trong cơ sở dữ liệu theo `prodId`
-    //         return Product.findByPk(prodId);
-    //     })
-    //     .then(product => {
-    //         // Sau khi đã có sản phẩm (có thể là sản phẩm đã có trong giỏ hàng hoặc sản phẩm mới),
-    //         // thêm sản phẩm vào giỏ hàng với số lượng mới (hoặc ban đầu là 1)
-    //         return fetchedCart.addProduct(product, {
-    //             through: {
-    //                 quantity: newQuantity // Ghi chú rằng đây là thông tin của bảng `CartItem` với cột `quantity`
-    //             }
-    //         });
-    //     })
-    //     .then(() => {
-    //         // Sau khi đã thêm sản phẩm vào giỏ hàng, chuyển hướng người dùng về trang giỏ hàng
-    //         res.redirect('/cart');
-    //     })
-    //     .catch(err => {
-    //         // Xử lý lỗi nếu có và in ra console
-    //         console.log(err);
-    //     });
 };
 
 
 exports.postCartDeleteProduct = (req, res, next) => {
-    const prodID = req.body.productId;  // Lấy productId từ dữ liệu gửi lên qua form (POST request)
-
-    req.user.getCart()  // Tìm giỏ hàng hiện tại của người dùng
-        .then(cart => {
-            // Tìm sản phẩm trong giỏ hàng có ID = prodID
-            return cart.getProducts({ where: { id: prodID } });
-        })
-        .then(products => {
-            const product = products[0];  // Lấy sản phẩm đầu tiên (vì chỉ có một sản phẩm có ID này trong giỏ)
-            // Xóa sản phẩm này khỏi bảng CartItem, nghĩa là xóa sản phẩm khỏi giỏ hàng
-            return product.cartItem.destroy();
-        })
+    const prodID = req.body.productId;
+    req.user.deleteItemFromCart(prodID)
         .then(result => {
             // Sau khi xóa thành công, chuyển hướng về trang giỏ hàng
             res.redirect('/cart');
@@ -151,30 +83,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
-
-    req.user.getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts(); // Lấy các sản phẩm từ giỏ hàng
-        })
-        .then(products => {
-            if (!products || products.length === 0) {  // Kiểm tra nếu products không tồn tại hoặc rỗng
-                console.log('No products in the cart.');
-                return res.redirect('/cart');  // Quay lại trang giỏ hàng nếu không có sản phẩm
-            }
-            return req.user.createOrder()
-                .then(order => {
-                    return order.addProducts(products.map(product => {
-                        product.orderItem = { quantity: product.cartItem.quantity };  // Thêm quantity vào orderItem
-                        return product;
-                    }));
-                });
-        })
-        .then(() => {
-            // Xóa tất cả sản phẩm khỏi giỏ hàng
-            return fetchedCart.setProducts(null);
-        })
+    req.user.addOrder()
         .then(() => {
             res.redirect('/orders'); // Chuyển hướng đến trang đơn hàng
         })
@@ -186,10 +95,9 @@ exports.postOrder = (req, res, next) => {
 
 
 exports.getOrders = (req, res, next) => {
-    req.user.getOrders( {include: ['products']})
+    req.user.getOrders()
         .then(
             orders => {
-                console.log(orders);
                 res.render('shop/orders', {
                     pageTitle: 'Your Orders',
                     path: '/orders',
--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ class User {
 
     addToCart(product) {
         const cartProductIndex = this.cart.items.findIndex(cp => {
-            return cp.productId.toString() === product._id.toString();
+            return cp.productId.toString() === product.id.toString();
         });
 
         let newQuantity = 1;
@@ -33,20 +33,112 @@ class User {
         if (cartProductIndex >= 0) {
             newQuantity = this.cart.items[cartProductIndex].quantity + 1;
             updatedCartItems[cartProductIndex].quantity = newQuantity;
-        }else {
-            updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity});
+        } else {
+            updatedCartItems.push({
+                productId: new ObjectId(product._id),
+                quantity: newQuantity,
+                addedAt: new Date() // Lưu thời gian thêm sản phẩm vào giỏ hàng
+            });
         }
-        const updateCart = {items : updatedCartItems};
+
+        const updateCart = { items: updatedCartItems };
 
         const db = getDb();
         return db.collection('users')
             .updateOne(
                 { _id: new ObjectId(this._id) },
-                {$set: {cart: updateCart}})
+                { $set: { cart: updateCart } }
+            )
             .then()
             .catch(err => {
                 console.log(err);
+            });
+    }
+
+    cleanCart() {
+        const maxAge = 30 * 60 * 1000; // 30 phút
+        const now = new Date();
+
+        this.cart.items = this.cart.items.filter(item => {
+            return now - new Date(item.addedAt) < maxAge; // Giữ lại sản phẩm còn trong thời gian quy định
+        });
+    }
+
+
+    getCart() {
+        this.cleanCart(); // Xóa sản phẩm quá hạn trước khi lấy giỏ hàng
+
+        const db = getDb();
+        const productIds = this.cart.items.map(i => {
+            return i.productId;
+        });
+
+        return db.collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then(products => {
+                return products.map(p => {
+                    return {
+                        ...p,
+                        quantity: this.cart.items.find(i => {
+                            return i.productId.toString() === p._id.toString();
+                        }).quantity
+                    };
+                });
             })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString();
+        });
+
+        const db = getDb();
+        return db.collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: { items: updatedCartItems } } }
+            )
+            .then(() => {
+                console.log('Product removed from cart');
+            })
+            .catch(err => {
+                console.log('Error removing product from cart:', err);
+            });
+    }
+
+    addOrder(){
+        const db = getDb();
+        return this.getCart().then(products =>{
+            const order = {
+                items: products,
+                user: {
+                    _id: new ObjectId(this._id),
+                    name: this.name,
+                }
+            }
+            return db.collection('orders')
+                .insertOne(order)
+        }).then(result =>{
+                this.cart = {items: []};
+                return db.collection('users')
+                    .updateOne(
+                        { _id: new ObjectId(this._id) },
+                        { $set: { cart: { items: [] } } }
+                    )
+            })
+            .catch(err =>{
+            console.log(err);
+        })
+    }
+
+    getOrders(){
+        const db = getDb();
+        return db.collection('orders').find({'user._id': new ObjectId(this._id)}).toArray();
     }
 
     static findById(id){
