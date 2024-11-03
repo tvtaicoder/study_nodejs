3fcbdb541980e6711aa7a921b3470ffcf5e13dcc
tvtaicoder <taikaka2015@gmail.com>
2024-11-03 11:52:32 +0700

    Chinh sua,xử lý lỗi

--- controllers/404.js
+++
@@ -1,7 +0,0 @@
-exports.get404Page = (req, res, next) => {
-    res.status(404).render('404', {
-        pageTitle: 'Not Found',
-        path: '/404',
-        isAuthenticated: req.isLoggedIn
-    });
-}
